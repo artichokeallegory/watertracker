9bd65cb9555a96a96f8885887979534ddbc913e8
artichokeallegory <cyncobb63@gmail.com>
2025-03-12 20:38:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let waterIntake = 0;
+let waterIntake = parseInt(localStorage.getItem("waterIntake")) || 0;
 const button = document.getElementById("log-water");
 const progressText = document.getElementById("progress-text");
 
 button.addEventListener("click", () => {
-    waterIntake += 8;
+    waterIntake += 8;  // Ensures 8 is treated as a number
     progressText.innerText = waterIntake + "oz logged today";
     new Audio("ding.mp3").play();
-    localStorage.setItem("waterIntake", waterIntake);
+    localStorage.setItem("waterIntake", waterIntake); // Save to local storage
 });
 
 window.onload = () => {
-    waterIntake = localStorage.getItem("waterIntake") || 0;
+    waterIntake = parseInt(localStorage.getItem("waterIntake")) || 0;
     progressText.innerText = waterIntake + "oz logged today";
 };
